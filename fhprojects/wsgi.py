035d2c87a77eed5975dfd80b7c40ded7c4a9b9e9
Stefan Reinhard <sr@feinheit.ch>
2012-07-18 19:12:48 +0200

    damit

--- fhprojects/wsgi.py
+++ fhprojects/wsgi.py
@@ -6,8 +6,6 @@ APP_BASEDIR = os.path.abspath(os.path.join(WEBAPP_DIR, os.path.pardir))
 if APP_BASEDIR not in sys.path:
     sys.path.insert(0, APP_BASEDIR)
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "example.settings")
-
 activate_this = os.path.expanduser(os.path.join(APP_BASEDIR, "venv", "bin", "activate_this.py"))
 execfile(activate_this, dict(__file__=activate_this))
 
