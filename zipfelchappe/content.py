39ea882a717d26d9f95abc2ebb4c613c3ed5ccb5
Stefan Reinhard <sr@feinheit.ch>
2012-07-20 02:35:21 +0200

    add simple teaser content type

---
+++ zipfelchappe/content.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.template.loader import render_to_string
+from django.utils.translation import ugettext_lazy as _
+
+from .models import Project
+
+class ProjectTeaserContent(models.Model):
+
+    project = models.ForeignKey(Project, verbose_name=_('project'),
+        related_name='teasercontents')
+
+    class Meta:
+        verbose_name = _('project teaser')
+        verbose_name_plural = _('project teasers')
+        abstract = True
+
+    def render(self, request, *args, **kwargs):
+        return render_to_string('zipfelchappe/project_teaser.html', {
+            'project': self.project,
+        })
