c998f54fa367b2c207af4d65e31a9b7d67607f7b
Stefan Reinhard <sr@feinheit.ch>
2012-08-09 17:05:06 +0200

    successfull and unsuccessfull mails

--- zipfelchappe/emails.py
+++ zipfelchappe/emails.py
@@ -1,9 +1,18 @@
 from django.core.mail import send_mail
 from django.contrib.sites.models import Site
 from django.db.models.signals import class_prepared, post_save
-from django.template.loader import render_to_string as render_str
+from django.template.loader import render_to_string
 
 from .models import Project, Update
+
+def render_mail(template, context):
+    subject = render_to_string('zipfelchappe/email/%s_subject.txt' % template,
+        context).strip()
+    message = rener_to_string('zipfelchappe/email/%s_message.txt' % template,
+        context)
+
+    return subject, message
+
 
 def send_update_mail(sender, instance, **kwargs):
     update = instance
@@ -22,13 +31,46 @@ def send_update_mail(sender, instance, **kwargs):
             'site': Site.objects.get_current()
         }
 
-        subject = render_str('zipfelchappe/emails/new_update_subject.txt', ctx).strip()
-        content = render_str('zipfelchappe/emails/new_update_content.txt', ctx)
+        subject, message = render_mail('new_update', ctx)
 
-        send_mail(subject, content, 'noreply@feinheit.ch',
+        send_mail(subject, message, 'noreply@feinheit.ch',
             [backer.email], fail_silently=False)
 
     update.mails_sent = True
     update.save()
 
 post_save.connect(send_update_mail, sender=Update)
+
+
+def send_successfull_message(project, pledge):
+
+    backer = pledge.backer
+
+    ctx = {
+        'project': project,
+        'pledge': pledge,
+        'backer': backer,
+        'site': Site.objects.get_current()
+    }
+
+    subject, message = render_mail('project_successfull', ctx)
+
+    send_mail(subject, message, 'noreply@feinheit.ch',
+        [backer.email], fail_silently=False)
+
+
+def send_unsuccessfull_message(project, pledge):
+
+    backer = pledge.backer
+
+    ctx = {
+        'project': project,
+        'pledge': pledge,
+        'backer': backer,
+        'site': Site.objects.get_current()
+    }
+
+    subject, message = render_mail('project_unsuccessfull', ctx)
+
+    send_mail(subject, message, 'noreply@feinheit.ch',
+        [backer.email], fail_silently=False)
--- zipfelchappe/paypal/management/commands/process_payments.py
+++ zipfelchappe/paypal/management/commands/process_payments.py
@@ -8,45 +8,46 @@ from zipfelchappe.models import Project, Pledge
 
 from zipfelchappe.paypal.models import Preapproval, Payment
 from zipfelchappe.paypal.paypal_api import create_payment
+from zipfelchappe import emails
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
-        projects = Project.objects.filter(end__lte=timezone.now()+timedelta(days=1))
-
+        projects = Project.objects.filter(end__lte=timezone.now())
 
         for project in projects:
-            print 'HANDLING PROJECT: %s' % project.title
+            if project.is_financed:
+                if not hasattr(project, 'receivers') or not project.receivers.count():
+                    print('Project %s has no paypal receivers!\n'
+                           'Please activate the extension and define at least one '
+                           'receiver in the admin.' % project)
+                    continue
 
-            if not hasattr(project, 'receivers') or not project.receivers.count():
-                print('Project %s has no paypal receivers!\n'
-                       'Please activate the extension and define at least one '
-                       'receiver in the admin.' % project)
-                continue
+                for pledge in project.pledges.filter(status=Pledge.AUTHORIZED):
+                    try:
+                        preapproval = pledge.preapproval
+                        if preapproval.status == 'ACTIVE' and preapproval.approved:
+                            pp_payment = create_payment(preapproval)
 
-            for pledge in project.pledges.filter(status=Pledge.AUTHORIZED):
-                try:
-                    preapproval = pledge.preapproval
-                    if preapproval.status == 'ACTIVE' and preapproval.approved:
-                        pp_payment = create_payment(preapproval)
+                            payment_json = json.dumps(pp_payment.json, indent=2)
 
-                        payment_json = json.dumps(pp_payment.json, indent=2)
+                            if 'error' in pp_payment.json:
+                                print "ERROR CREATING PAYMENT: "
+                                print payment_json
+                            else:
+                                payment = Payment.objects.create(
+                                    key = pp_payment.json['payKey'],
+                                    preapproval = preapproval,
+                                    status = pp_payment.json['paymentExecStatus'],
+                                    data = payment_json,
+                                )
 
-                        if 'error' in pp_payment.json:
-                            print "ERROR CREATING PAYMENT: "
-                            print payment_json
+                                print "PAYMENT FOR PLEDGE %s CREATED" % pledge
+
+                                mails.send_successfull_message(project, pledge)
                         else:
-                            payment = Payment.objects.create(
-                                key = pp_payment.json['payKey'],
-                                preapproval = preapproval,
-                                status = pp_payment.json['paymentExecStatus'],
-                                data = payment_json,
-                            )
+                            print "PLEDGE NOT APPROVED: %s" % pledge
 
-                            print "PAYMENT FOR PLEDGE %s CREATED" % pledge
-                    else:
-                        print "PLEDGE NOT APPROVED: %s" % pledge
-
-                except Preapproval.DoesNotExist:
-                    print "AUTHORIZED PLEDGE WITHOUT PREAPPROVAL FOUND"
+                    except Preapproval.DoesNotExist:
+                        pass
