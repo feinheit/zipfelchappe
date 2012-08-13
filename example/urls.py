d328379a645ad351471dce902d2582f987def06b
Stefan Reinhard <sr@feinheit.ch>
2012-08-13 14:05:06 +0200

    simply payment module integration

--- example/urls.py
+++ example/urls.py
@@ -3,21 +3,15 @@ from django.conf.urls.defaults import *
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.generic.simple import redirect_to
-from django.shortcuts import redirect
 
 admin.autodiscover()
-
-def zipfelchappe_payment(request):
-    return redirect('zipfelchappe_paypal_payment')
 
 urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^account/', include('django.contrib.auth.urls')),
     url(r'^$', redirect_to, {'url':'/projects/'}),
-    url(r'^zipfelchappe/paypal/', include('zipfelchappe.paypal.urls')),
-    url(r'^zipfelchappe/payment/$', zipfelchappe_payment,
-        name='zipfelchappe_payment'),
+    url(r'^zipfelchappe/payment/', include('zipfelchappe.paypal.urls')),
     url(r'', include('feincms.urls')),
 )
 
