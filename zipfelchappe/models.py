2dd73569cf82c20c403fabc693feee1c84c8216e
Stefan Reinhard <sr@feinheit.ch>
2012-10-04 16:54:11 +0200

    use firstname or lastname if only one

--- zipfelchappe/models.py
+++ zipfelchappe/models.py
@@ -57,7 +57,7 @@ class BackerBase(models.Model):
 
     @property
     def full_name(self):
-        if self.first_name and self.last_name:
+        if self.first_name or self.last_name:
             return u'%s %s' % (self.first_name, self.last_name)
         else:
             return unicode(self.user)
