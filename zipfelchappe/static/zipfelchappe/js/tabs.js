06014a888364d81071e77b675a974444ac1b8550
Stefan Reinhard <sr@feinheit.ch>
2013-03-05 12:14:37 +0100

    prevent scrolling on tab switches

--- zipfelchappe/static/zipfelchappe/js/tabs.js
+++ zipfelchappe/static/zipfelchappe/js/tabs.js
@@ -1,11 +1,14 @@
 $(function () {
     $('.nav-tabs a').click(function (e) {
-      window.location.hash = $(this).attr('href');
+      document.location.hash = $(this).attr('href');
       $(this).tab('show');
-    })
+      return false;
+    });
 
-    if (window.location.hash === '') {
-        $('.nav-tabs a:first').tab('show');
+    if (document.location.hash === '') {
+        var $first = $('.nav-tabs a:first');
+        document.location.hash = $first.attr('href');
+        $first.tab('show');
     } else {
         $('.nav-tabs a[href="'+window.location.hash+'"]').tab('show');
     }
