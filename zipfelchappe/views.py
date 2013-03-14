ad9b0881b0104a69cccd7741d12b94390cc9b25f
Stefan Reinhard <sr@feinheit.ch>
2013-03-14 11:24:23 +0100

    remove unnecessary code since feincms 1.7

--- zipfelchappe/views.py
+++ zipfelchappe/views.py
@@ -14,6 +14,7 @@ from django.utils.translation import ugettext_lazy as _
 from django.views.decorators.csrf import csrf_exempt
 
 from feincms.content.application.models import app_reverse
+from feincms.module.mixins import ContentObjectMixin
 
 from . import forms, app_settings
 from .emails import send_pledge_completed_message
@@ -145,25 +146,13 @@ class ProjectCategoryListView(FeincmsRenderMixin, ListView):
         return context
 
 
-class ProjectDetailView(FeincmsRenderMixin, DetailView):
+class ProjectDetailView(DetailView, ContentObjectMixin):
 
     context_object_name = "project"
     model = Project
 
     def get_queryset(self):
         return Project.objects.online().select_related('backers', 'pledges')
-
-    def get(self, request, *args, **kwargs):
-        self.object = self.get_object()
-        response = self.prepare()
-        if response:
-            return response
-
-        response = self.render_to_response(self.get_context_data(object=self.object))
-        return self.finalize(response)
-
-    def post(self, request, *args, **kwargs):
-        return self.get(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         context = super(ProjectDetailView, self).get_context_data(**kwargs)
@@ -177,50 +166,6 @@ class ProjectDetailView(FeincmsRenderMixin, DetailView):
         context['pledge'] = get_object_or_none(Pledge, pk=pledge_id)
 
         return context
-
-    def prepare(self):
-        """
-        Prepare / pre-process content types. If this method returns anything,
-        it is treated as a ``HttpResponse`` and handed back to the visitor.
-        """
-
-        http404 = None      # store eventual Http404 exceptions for re-raising,
-                            # if no content type wants to handle the request
-        successful = False  # did any content type successfully end processing?
-
-        contents = tuple(self.object._feincms_content_types_with_process)
-        for content in self.object.content.all_of_type(contents):
-            try:
-                r = content.process(self.request, view=self)
-                if r in (True, False):
-                    successful = r
-                elif r:
-                    return r
-            except Http404, e:
-                http404 = e
-
-        if not successful:
-            if http404:
-                # re-raise stored Http404 exception
-                raise http404
-
-    def finalize(self, response):
-        """
-        Runs finalize() on content types having such a method, adds headers and
-        returns the final response.
-        """
-
-        if not isinstance(response, HttpResponse):
-            # For example in the case of inheritance 2.0
-            return response
-
-        contents = tuple(self.object._feincms_content_types_with_finalize)
-        for content in self.object.content.all_of_type(contents):
-            r = content.finalize(self.request, response)
-            if r:
-                return r
-
-        return response
 
 
 class UpdateDetailView(FeincmsRenderMixin, DetailView):
