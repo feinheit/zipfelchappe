d454ddf95f2368dda518af537160ce47d2a3857e
Stefan Reinhard <sr@feinheit.ch>
2013-04-03 12:26:30 +0200

    2013

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'zipfelchappe'
-copyright = u'2012, Stefan Reinhard'
+copyright = u'2013, Stefan Reinhard'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
